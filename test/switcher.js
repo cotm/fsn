f044c34a99cd6dbc4b32bd3389c43b354cf002de
Todd Yandell <tyandell@gmail.com>
2013-04-21 13:24:46 -0500

    Fix tests and remove logging

--- test/switcher.js
+++ test/switcher.js
@@ -11,7 +11,7 @@ var switcher = new Switcher({
   asyncPort: 3001
 });
 
-xdescribe('fsn.Switcher', function () {
+describe('fsn.Switcher', function () {
   describe('Switcher#scope', function () {
     it('returns an fsn.Scope', function () {
       switcher.scope('MECard:0').should.be.an.instanceOf(Scope);
@@ -20,65 +20,72 @@ xdescribe('fsn.Switcher', function () {
   
   describe('Switcher#send', function () {
     it('return response', function (done) {
-      switcher.send(new Node('Frame:0'), done);
+      switcher.send(new Node('Frame:0'), checkMessage(done));
     });
     
     it('emit `message`', function (done) {
       switcher.send(new Node('Frame:0'));
-      switcher.once('message', done);
+      switcher.once('message', checkMessage(done));
     });
   });
   
   describe('Switcher#query', function () {
     it('return response', function (done) {
-      switcher.query(done);
+      switcher.query(checkMessage(done));
     });
     
     it('emit `message`', function (done) {
       switcher.query();
-      switcher.once('message', done);
+      switcher.once('message', checkMessage(done));
     });
   });
   
   describe('Switcher#set', function () {
     it('return response (Object)', function (done) {
-      switcher.set({'Test': 1, 'Test/Test': 2}, done);
+      switcher.set({'Test': 1, 'Test/Test': 2}, checkMessage(done));
     });
     
     it('emit `message` (Object)', function (done) {
       switcher.set({'Test': 1, 'Test/Test': 2});
-      switcher.once('message', done);
+      switcher.once('message', checkMessage(done));
     });
     
     it('return response (path, value)', function (done) {
-      switcher.set('Test', 1, done);
+      switcher.set('Test', 1, checkMessage(done));
     });
     
     it('emit `message` (path, value)', function (done) {
       switcher.set('Test', 1);
-      switcher.once('message', done);
+      switcher.once('message', checkMessage(done));
     });
   });
   
   describe('Switcher#action', function () {
     it('return response', function (done) {
-      switcher.action('Test', done);
+      switcher.action('Test', checkMessage(done));
     });
     
     it('emit `message`', function (done) {
       switcher.action('Test');
-      switcher.once('message', done);
+      switcher.once('message', checkMessage(done));
     });
   });
   
   describe('Switcher#source', function () {
     it('return response', function (done) {
-      switcher.source('Test', source.xpt(1), done);
+      switcher.source('Test', source.xpt(1), checkMessage(done));
     });
     
     it('emit `message`', function (done) {
       switcher.source('Test', source.xpt(1));
-      switcher.once('message', done);
+      switcher.once('message', checkMessage(done));
     });
   });
+  
+  function checkMessage (callback) {
+    return function (message) {
+      message.should.be.instanceOf(Node);
+      callback();
+    }
+  }
 });
