b855af43516021a8fc10cf74253b109127685cda
Todd Yandell <tyandell@gmail.com>
2013-05-05 01:23:11 -0500

    Test for Parser

---
+++ test/parser.js
@@ -0,0 +1,51 @@
+var should = require('should');
+
+var Parser = require('../lib/parser').Parser,
+    Node = require('../lib/node').Node;
+
+describe('Parser', function () {
+  var parser = new Parser();
+  
+  it('should emit `open`', function (done) {
+    parser.once('open', function (name, num) {
+      name.should.eql('A');
+      num.should.eql(1);
+      done();
+    });
+    parser.write('<A Num="1"></A>');
+  });
+  
+  it('should emit `close`', function (done) {
+    parser.once('close', function (name) {
+      name.should.eql('B');
+      done();
+    });
+    parser.write('<B></B>');
+  });
+  
+  it('should emit `action`', function (done) {
+    parser.once('action', function (name) {
+      name.should.eql('C');
+      done();
+    });
+    parser.write('<C/>');
+  });
+  
+  it('should emit `text`', function (done) {
+    parser.once('text', function (text) {
+      text.should.eql('test');
+      done();
+    });
+    parser.write('<D>test</D>');
+  });
+  
+  it('should emit `message`', function (done) {
+    parser.once('message', function (message) {
+      message.should.be.instanceOf(Node);
+      should.exist(message.child('B:0', false));
+      should.exist(message.child('B:0/C:0', false));
+      done();
+    });
+    parser.write('<A Num="0"><B Num="0"><C Num="0"></C></B></A>');
+  });
+});
