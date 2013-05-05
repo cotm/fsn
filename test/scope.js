3096e113f0bcfb97d2e997e0c1646058405b1e76
Todd Yandell <tyandell@gmail.com>
2013-05-05 02:42:10 -0500

    Scope test improvements

--- test/scope.js
+++ test/scope.js
@@ -3,115 +3,79 @@ var should = require('should'),
 
 var Scope = require('../lib/scope').Scope;
 
-var switcher = {
-  scope: function (prefix) {
-    return new Scope(switcher, prefix);
-  },
-  
-  query: function () {},
-  set: function () {},
-  action: function () {},
-  source: function () {}
-};
-
-describe('fsn.Scope', function () {
-  var scope = switcher.scope('Test');
-  
-  it('has fsn.Switcher methods', function () {
-    var switcher = require('../lib/switcher').Switcher.prototype;
-    
-    var exceptMethods = ['end', 'send'];
-    
-    for (var i in switcher) {
-      if (!switcher.hasOwnProperty(i)) continue;
-      if (typeof switcher[i] != 'function') continue;
-      if (exceptMethods.indexOf(i) != -1) continue;
-      
-      scope[i].should.be.a('function');
-      scope[i].length.should.eql(switcher[i].length);
-    }
-  });
-  
-  describe('Scope#query', function () {
-    it('add prefix to path', function () {
-      var mock = sinon.mock(switcher);
-      mock.expects('query').withArgs('Test/A');
-      scope.query('A');
-      mock.verify();
-      
-      mock = sinon.mock(switcher);
-      mock.expects('query').withArgs('Test/A');
-      scope.query('A', true);
-      mock.verify();
-    });
-    
-    it('no trailing slash without explicit path', function () {
-      var mock = sinon.mock(switcher);
-      mock.expects('query').withArgs('Test');
-      scope.query();
-      mock.verify();
-      
-      mock = sinon.mock(switcher);
-      mock.expects('query').withArgs('Test');
-      scope.query(true);
-      mock.verify();
+describe('Scope', function () {
+  describe('#scope()', function () {
+    it('should return another Scope', function () {
+      var a = new Scope(null, 'A');
+      var b = a.scope('B');
+      b.should.be.instanceOf(Scope);
+      b.parent.should.equal(a);
     });
   });
   
-  describe('Scope#set', function () {
-    it('add prefix to each path (Object)', function () {
-      var mock = sinon.mock(switcher);
-      mock.expects('set').withArgs({'Test/A': 1, 'Test/B': 2});
-      scope.set({A: 1, B: 2});
-      mock.verify();
-    });
-    
-    it('add prefix to each path (path, value)', function () {
-      var mock = sinon.mock(switcher);
-      mock.expects('set').withArgs({'Test/A': 1});
-      scope.set('A', 1);
-      mock.verify();
-    });
+  it('should prefix #query()', function () {
+    var switcher = {query: sinon.spy()};
+    var scope = new Scope(switcher, 'A');
+    scope.query();
+    scope.query(function () {});
+    scope.query(true);
+    scope.query(true, function () {});
+    should(switcher.query.alwaysCalledWith('A', true));
   });
   
-  describe('Scope#action', function () {
-    it('add prefix to path', function () {
-      var mock = sinon.mock(switcher);
-      mock.expects('action').withArgs('Test/Cut');
-      scope.action('Cut');
-      mock.verify();
-    });
+  it('should prefix #query(path)', function () {
+    var switcher = {query: sinon.spy()};
+    var scope = new Scope(switcher, 'A');
+    scope.query('B');
+    scope.query('B', function () {});
+    scope.query('B', true);
+    scope.query('B', true, function () {});
+    should(switcher.query.alwaysCalledWith('A/B', true));
   });
   
-  describe('Scope#source', function () {
-    it('add prefix to path', function () {
-      var mock = sinon.mock(switcher);
-      mock.expects('source').withArgs('Test/Src:0');
-      scope.source('Src:0', null);
-      mock.verify();
-    });
+  it('should prefix #watch()', function () {
+    var switcher = {watch: sinon.spy()};
+    var scope = new Scope(switcher, 'A');
+    scope.watch(function () {});
+    should(switcher.watch.calledWith('A'));
   });
   
-  describe('Scope#scope', function () {
-    var scopeB = scope.scope('B'),
-        scopeC = scopeB.scope('C');
-    
-    it('nest one level deep', function () {
-      scopeB.parent.should.equal(scope);
-      
-      var mock = sinon.mock(switcher);
-      mock.expects('set').withArgs({'Test/B/X': 1});
-      scopeB.set({X: 1});
-      mock.verify();
-    });
-    
-    it('nest two levels deep', function () {
-      scopeC.parent.should.equal(scopeB);
-      
-      var mock = sinon.mock(switcher);
-      mock.expects('set').withArgs({'Test/B/C/X': 1});
-      scopeC.set({X: 1});
-      mock.verify();
-    });
+  it('should prefix #watch(path)', function () {
+    var switcher = {watch: sinon.spy()};
+    var scope = new Scope(switcher, 'A');
+    scope.watch('B', function () {});
+    should(switcher.watch.calledWith('A/B'));
+  });
+  
+  it('should prefix #set(Object)', function () {
+    var switcher = {set: sinon.spy()};
+    var scope = new Scope(switcher, 'A');
+    scope.set({B: 1, C: 2});
+    scope.set({B: 1, C: 2}, function () {});
+    should(switcher.set.alwaysCalledWith({'A/B': 1, 'A/C': 2}));
+  });
+  
+  it('should prefix #set(path, value)', function () {
+    var switcher = {set: sinon.spy()};
+    var scope = new Scope(switcher, 'A');
+    scope.set('B', 1);
+    scope.set('B', 1, function () {});
+    should(switcher.set.alwaysCalledWith({'A/B': 1}));
+  });
+  
+  it('should prefix #action()', function () {
+    var switcher = {action: sinon.spy()};
+    var scope = new Scope(switcher, 'A');
+    scope.action('B');
+    scope.action('B', function () {});
+    should(switcher.action.alwaysCalledWith('A/B'));
+  });
+  
+  it('should prefix #source()', function () {
+    var switcher = {source: sinon.spy()};
+    var scope = new Scope(switcher, 'A');
+    scope.source('B', null);
+    scope.source('B', null, function () {});
+    should(switcher.source.alwaysCalledWith('A/B'));
   });
 });
